0146c67c68373c507c96d0c6d03d3c6fd593a31d
WaseemH <w927hussain@gmail.com>
2023-12-20 11:18:35 -0500

    small change to url

--- test_main.py
+++ test_main.py
@@ -6,12 +6,12 @@ client = TestClient(app)
 
 
 def test_prompt():
-    response = client.post("/prompt/", json={"message": "Hello AI!"})
+    response = client.post("/prompt", json={"message": "Hello AI!"})
     assert response.status_code == 200
     assert "response" in response.json()
 
 
 def test_prompt_stream():
-    response = client.post("/prompt/stream/", json={"message": "Hello AI!"})
+    response = client.post("/prompt/stream", json={"message": "Hello AI!"})
     assert response.status_code == 200
     assert response.headers["content-type"] == "text/event-stream; charset=utf-8"
